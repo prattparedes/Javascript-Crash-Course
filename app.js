f0eee8b5c7d26312d3be439ccf02976f1f50ec3b
Pratt <pratt.paredes@gmail.com>
2022-09-10 16:45:50 -0500

    primer ejercicio

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-alert('rawr')
+console.log('Hello World'[10])
+console.log('queeeeeeeee'.length)
+
+let str = 'Holiwis'
+console.log(str.length)
+console.log(str[str.length-1])
+
+let a = 'raa'
+
+console.log(typeof(a))
+
+
+console.log(11%2)
+console.log(5<10)
+
+let name = 'Pratt'
+let fullName = 'Pratt Paredes'
+
+let isRaining = true
+let temperature = 30
+let planet = 'Tierra'
+
+temperature = temperature + 5
+
+console.log(temperature)
+
+const años = 23
+
+let añosluz = años + 5
+console.log(añosluz)
+
+
+/**
+ * Convirtiendo temperatura
+ * 
+ * Formula de celsius a fahrenheit:
+ * 
+ * F= C x 1.8 + 32
+ */
+
+let celsius = 24;
+let fahrenheit = celsius*1.8 + 32      /* Convierte celsius a fahrenheit */
+
+console.log(fahrenheit)
+alert(celsius + ' celsius convertido a fahrenheit es ' + fahrenheit)  /*print out result */
